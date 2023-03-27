0227dafbedd2bdcb420331539ed6e8d919d38f9f
Angelina Polyakova <lina.krasilnikova.33@gmail.com>
2023-03-27 17:21:38 +0400

    fixed bags, added slider and accordion

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-$('.burger').on('click', function() {
-    $('.navigation').fadeToggle();
-    $('.callback').fadeToggle();
+$(".burger").on("click", function () {
+  $(".navigation").fadeToggle();
+  $(".callback").fadeToggle();
+});
 
-})
+$(".js-slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  variableWidth: true,
+  slidesToScroll: 1,
+  rows: 0,
+  prevArrow: $(".arrows__prev"),
+  nextArrow: $(".arrows__next"),
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".faq__accordion").accordion();
